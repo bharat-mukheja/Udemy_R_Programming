8b94cef6dbde4d294163921764d19df1bfc1587a
Bharat Mukheja <bharat.mukheja@gmail.com>
2021-10-05 00:30:48 -0700

    section 2 start

---
+++ Section_2/types_of_variables.R
@@ -0,0 +1,21 @@
+## All data types
+
+#integers, need to suffix 'L' to define an integer
+x <- 2L
+typeof(x)
+
+#double
+y <- 2.5
+typeof(y)
+
+#complex
+z <- 3+2i
+typeof(z)
+
+#character
+a <- "n"
+typeof(a)
+
+#logical, can be single char or full word, both IN CAPS e.g T or TRUE
+b <- T
+typeof(b)
