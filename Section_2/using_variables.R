54d59858be67d5a711e97799af24b3149294b279
Bharat Mukheja <bharat.mukheja@gmail.com>
2021-10-05 00:30:57 -0700

    using_variables

---
+++ Section_2/using_variables.R
@@ -0,0 +1,19 @@
+A <- 10
+B <- 5
+
+C <- A + B
+D <- A * B
+E <- A / B
+
+# To print any variable, just write its name
+C
+
+var1<-2.5
+var2<-4
+var1*var2
+
+ans <- sqrt(var2)
+
+greeting <- "Hello"
+name <- "Bob"
+message<-paste(greeting,name)
