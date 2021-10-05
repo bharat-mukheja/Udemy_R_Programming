8d7a9dfa25b9a3084717ec57c6ed5bb79cbf8af2
Bharat Mukheja <bharat.mukheja@gmail.com>
2021-10-05 11:28:36 -0700

    logical vars

---
+++ Section_2/logical_variables_operators.R
@@ -0,0 +1,29 @@
+##Logical:
+
+#variables
+#TRUE T
+#FALSE F
+4 < 5
+10 > 100
+4 == 5
+
+#operators
+# ==
+# !=
+# <
+# >
+# <=
+# >=
+# !
+# |
+# &
+# isTRUE(x)
+
+result <- 4 < 5
+
+result2 <- !TRUE
+
+result | result2
+result & result2
+
+isTRUE(result)
