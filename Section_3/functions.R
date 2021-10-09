d55273aac08d8a149b348b954831c4a0bbcfc754
Bharat Mukheja <bharat.mukheja@gmail.com>
2021-10-09 15:54:19 -0700

    functions example

---
+++ Section_3/functions.R
@@ -0,0 +1,26 @@
+
+#All functions used until now
+rnorm()
+
+c()
+seq()
+rep()
+
+print()
+
+is.numeric()
+is.integer()
+is.double()
+is.character()
+
+typeof()
+
+sqrt()
+paste()
+
+#Call help using ? in front of function name
+?sqrt
+?rnorm
+
+# Arguments with defined values in function help mean default variables
+# named variables can be passed in any order
